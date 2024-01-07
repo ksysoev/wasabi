78ac139ccd6b18d06a0fb2926b28c15ffae06593
Кирилл Сысоев <k.sysoev@me.com>
2024-01-07 11:54:46 +0800

    Adds request middleware support to dipatcher

--- backend.go
+++ backend.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Backend interface {
-	Forward(conn *Connection, r Request) error
+	Handle(conn *Connection, r Request) error
 }
 
 type HttpBackend struct {
@@ -18,7 +18,7 @@ func NewBackend(endpoint string) *HttpBackend {
 	return &HttpBackend{endpoint: endpoint}
 }
 
-func (b *HttpBackend) Forward(conn *Connection, r Request) error {
+func (b *HttpBackend) Handle(conn *Connection, r Request) error {
 	req, ok := r.(*JSONRPCRequest)
 	if !ok {
 		return nil
--- dispatcher.go
+++ dispatcher.go
@@ -5,13 +5,32 @@ type Dispatcher interface {
 }
 
 type PipeDispatcher struct {
-	backend Backend
+	backend     Backend
+	middlewares []RequestMiddlewere
 }
+
+type RequestHandler interface {
+	Handle(conn *Connection, req Request) error
+}
+
+type RequestMiddlewere func(next RequestHandler) RequestHandler
 
 func NewPipeDispatcher(backend Backend) *PipeDispatcher {
 	return &PipeDispatcher{backend: backend}
 }
 
 func (d *PipeDispatcher) Dispatch(conn *Connection, req Request) error {
-	return d.backend.Forward(conn, req)
+	return d.useMiddleware(d.backend).Handle(conn, req)
+}
+
+func (d *PipeDispatcher) Use(middlewere RequestMiddlewere) {
+	d.middlewares = append(d.middlewares, middlewere)
+}
+
+func (d *PipeDispatcher) useMiddleware(endpoint RequestHandler) RequestHandler {
+	for i := len(d.middlewares) - 1; i >= 0; i-- {
+		endpoint = d.middlewares[i](endpoint)
+	}
+
+	return endpoint
 }
