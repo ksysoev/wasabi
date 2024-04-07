af6860bc4ce13382eb9c12fd7c21a9974bceeb2f
Kirill Sysoev <k.sysoev@me.com>
2024-04-07 19:42:01 +0800

    Adds tests for http backend

---
+++ backend/http_test.go
@@ -0,0 +1,38 @@
+package backend
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ksysoev/wasabi"
+	"github.com/ksysoev/wasabi/mocks"
+)
+
+func TestHTTPBackend_Handle(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`OK`))
+	}))
+	defer server.Close()
+
+	mockConn := mocks.NewMockConnection(t)
+	mockReq := mocks.NewMockRequest(t)
+
+	mockConn.EXPECT().Send([]byte("OK")).Return(nil)
+	mockReq.EXPECT().Data().Return([]byte("test request"))
+
+	backend := NewBackend(func(req wasabi.Request) (*http.Request, error) {
+		bodyReader := bytes.NewBufferString(string(req.Data()))
+		httpReq, err := http.NewRequest("GET", server.URL, bodyReader)
+		if err != nil {
+			return nil, err
+		}
+		return httpReq, nil
+	})
+
+	err := backend.Handle(mockConn, mockReq)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
