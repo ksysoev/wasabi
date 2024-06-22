d4c0120b994d3c3e8b357e157ebc312249c12391
Kirill Sysoev <k.sysoev@me.com>
2024-06-22 16:42:33 +0800

    feat: Add unit tests for retry middleware

---
+++ middleware/request/retry_test.go
@@ -0,0 +1,72 @@
+package request
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ksysoev/wasabi"
+	"github.com/ksysoev/wasabi/dispatch"
+	"github.com/ksysoev/wasabi/mocks"
+)
+
+func TestNewRetryMiddleware(t *testing.T) {
+	maxRetries := 3
+	interval := time.Microsecond
+	middleware := NewRetryMiddleware(maxRetries, interval)
+
+	// Create a mock request handler
+	mockHandler := dispatch.RequestHandlerFunc(func(conn wasabi.Connection, req wasabi.Request) error {
+		return fmt.Errorf("mock error")
+	})
+
+	ctx := context.Background()
+
+	// Create a mock connection and request
+	mockConn := mocks.NewMockConnection(t)
+	mockReq := mocks.NewMockRequest(t)
+
+	mockReq.EXPECT().Context().Return(ctx)
+
+	// Test with successful request
+	mockHandlerSuccess := dispatch.RequestHandlerFunc(func(conn wasabi.Connection, req wasabi.Request) error {
+		return nil
+	})
+	err := middleware(mockHandlerSuccess).Handle(mockConn, mockReq)
+	if err != nil {
+		t.Errorf("Expected no error, but got %v", err)
+	}
+
+	// Test with failed request
+	err = middleware(mockHandler).Handle(mockConn, mockReq)
+	if err == nil {
+		t.Error("Expected error, but got nil")
+	}
+}
+
+func TestNewRetryMiddleware_CancelledContext(t *testing.T) {
+	maxRetries := 3
+	interval := time.Microsecond
+	middleware := NewRetryMiddleware(maxRetries, interval)
+
+	// Create a mock request handler
+	mockHandler := dispatch.RequestHandlerFunc(func(conn wasabi.Connection, req wasabi.Request) error {
+		return fmt.Errorf("mock error")
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	// Create a mock connection and request
+	mockConn := mocks.NewMockConnection(t)
+	mockReq := mocks.NewMockRequest(t)
+
+	mockReq.EXPECT().Context().Return(ctx)
+
+	// Test with failed request
+	err := middleware(mockHandler).Handle(mockConn, mockReq)
+	if err != context.Canceled {
+		t.Errorf("Expected error to be context.Canceled, but got %v", err)
+	}
+}
