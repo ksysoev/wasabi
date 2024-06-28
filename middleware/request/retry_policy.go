551e5958f5a9dee6ffc4abb6b08719d1aa46752b
shan-96 <shantanumh18ab3632@gmail.com>
2024-06-28 17:07:57 +0700

    add godoc

--- middleware/request/retry_policy.go
+++ middleware/request/retry_policy.go
@@ -5,14 +5,20 @@ import (
 	"time"
 )
 
+// GetRetryInterval is a higher order function that returns a function to get retry interval.
+// The inner function takes the retry interation (int) as an argument and returns the duration until next retry
 type GetRetryInterval func(int) time.Duration
 
+// ExponentialGetRetryInterval is an exponential backoff implementation of GetRetryInterval.
+// It enables the retry middleware to ensure exponentially higher wait times between retries.
 func ExponentialGetRetryInterval(seed time.Duration, delayFactor int) GetRetryInterval {
 	return func(iteration int) time.Duration {
 		return seed * (time.Duration(math.Pow(float64(delayFactor), float64(iteration))))
 	}
 }
 
+// LinearGetRetryInterval is an linear delay implementation of GetRetryInterval.
+// It enables the retry middleware to ensure constant wait times between retries.
 func LinearGetRetryInterval(interval time.Duration) GetRetryInterval {
 	return func(_ int) time.Duration {
 		return interval
