27449e350492254c81c0b75ebc8de3b4c9518e6f
shan-96 <shantanumh18ab3632@gmail.com>
2024-06-30 23:28:33 +0700

    godoc

--- middleware/request/retry.go
+++ middleware/request/retry.go
@@ -10,7 +10,7 @@ import (
 // NewRetryMiddleware returns a new retry middleware that wraps the provided `next` request handler.
 // The middleware accepts 2 params consisting of:
 //   - `getRetryInterval`: A higher order function that returns a function to calculate next retry interval
-//   - `shouldRetry`: A higher order function that returns a function to calculate if we should retry again or not
+//   - `maxRetries`: maximum number of retries allowed for this middleware
 //
 // If the request succeeds at any retry, the middleware returns `nil`. If all retries fail, it returns the last error encountered.
 func NewRetryMiddleware(getRetryInterval GetRetryInterval, maxRetries int) func(next wasabi.RequestHandler) wasabi.RequestHandler {
